e7efe71c6e3970d3279a7b900fda2d177ded5a99
rn <ricky.ariansyah@menara-astra.co.id>
2021-08-20 22:29:11 +0700

    brief commit

---
+++ brief-ch5/brief.js
@@ -0,0 +1,20 @@
+// express generator options --ejs
+// pindahin ch3 - ch4
+
+// ada 2 enpoint
+  // /home
+  // /game
+
+// buat middleware untuk memvalidasi user
+// localhost:3000/game?nama=Ricky
+
+// dokumentasikan ke postman
+
+// buat api serving user
+ // getalluser
+ // getdetailuser
+ 
+
+
+
+
